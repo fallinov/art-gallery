bb03a94f90e0347d2026369030841920b434012e
Steve Fallet <steve.fallet@gmail.com>
2015-09-07 16:19:08 +0200

    -first commit -renommer .htlm > .php

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Steve Fallet | Designer</title>
+		<!-- Polices -->
+		<link href='http://fonts.googleapis.com/css?family=Changa+One|Open+Sans:400italic,700italic,400,700,800' rel='stylesheet' type='text/css'>
+		<!-- Styles -->
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/responsive.css">
+
+	</head>
+	<body>
+		<!-- ENTETE DU SITE -->
+		<header>
+			<a id="logo" href="index.html">
+				<h1>Steve Fallet</h1>
+				<h2>Designer</h2>
+			</a><nav>
+				<ul>
+					<li><a href="index.html" class="selected">Home</a></li>
+					<li><a href="about.html">About</a></li>
+					<li><a href="contact.html">Contact</a></li>
+				</ul>
+			</nav>
+		</header>
+		<!-- regoupe ccontenu et pied de page -->
+		<div id="wrapper">
+			<!-- GALLERIE -->	
+			<section>
+				<h2 class="cache">Mes réalisations</h2>
+				<ul id="gallerie">
+					<li>
+						<a href="img/numbers-01.jpg" title="Voir le tableau chiffre 1">
+							<img src="img/numbers-01.jpg" alt="Tableau le chiffre 1" />
+							<p>Expérimentation de couleurs</p>
+						</a>
+					</li><li>
+						<a href="img/numbers-02.jpg" title="Voir le tableau chiffre 2">
+							<img src="img/numbers-02.jpg" alt="Tableau le chiffre 2" />
+							<p>Jeu de fondus</p>
+						</a>
+					</li><li>
+						<a href="img/numbers-06.jpg" title="Voir le tableau chiffre 6">
+							<img src="img/numbers-06.jpg" alt="Tableau le chiffre 6" />
+							<p>80's style</p>
+						</a>
+					</li><li>
+						<a href="img/numbers-09.jpg" title="Voir le tableau chiffre 9">
+							<img src="img/numbers-09.jpg" alt="Tableau le chiffre 9" />
+							<p>Travail avec les pinceaux</p>
+						</a>
+					</li><li>
+						<a href="img/numbers-12.jpg" title="Voir le tableau numéro 12">
+							<img src="img/numbers-12.jpg" alt="Tableau le numéro 12" />
+							<p>Répétition de formes</p>
+						</a>
+					</li> <!-- fin tableau 12 -->
+				</ul>
+			</section> <!-- fin section principale -->
+			<!-- PEID DE PAGE -->
+			<footer>
+				<ul>
+					<li>
+						<a href="http://twitter.com/fallinov" title="Mon twitter">
+							<img src="img/twitter-wrap.png" alt="Twitter Logo"/>
+						</a>
+					</li>
+					<li>
+						<a href="http://facebook.com/steve-fallet" title="Ma page facebook">
+							<img src="img/facebook-wrap.png" alt="Facebook Logo"/>
+						</a>
+					</li>
+				</ul>
+				<p>&copy;2014 Steve Fallet</p>
+			</footer>
+		</div>
+	</body>
+</html>
