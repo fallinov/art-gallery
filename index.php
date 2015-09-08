57ebbb6c587d18d1a705dafb3634efcbc28ca36e
Steve Fallet <steve.fallet@gmail.com>
2015-09-08 08:04:15 +0200

    - home page avec oeuvres dans stockées dans un tableau ¨- parcours du tableau

--- index.php
+++ index.php
@@ -3,11 +3,53 @@
 $pageTitle = "Accueil";
 $section = "home";
 
+$artWork  = array();
+
+$artWork[0] = array(
+	'titre' 		=> 'Tableau le chiffre 1',
+	'image' 		=> 'numbers-01.jpg',
+	'description' 	=> 'Expérimentation de couleurs');
+
+$artWork[1] = array(
+	'titre' 		=> 'Tableau le chiffre 2',
+	'image' 		=> 'numbers-02.jpg',
+	'description' 	=> 'Jeu de fondus');
+
+$artWork[2] = array(
+	'titre' 		=> 'Tableau le chiffre 6',
+	'image' 		=> 'numbers-06.jpg',
+	'description' 	=> '80\'s style');
+
+$artWork[3] = array(
+	'titre' 		=> 'Tableau le chiffre 9',
+	'image' 		=> 'numbers-09.jpg',
+	'description' 	=> 'Travail avec les pinceaux');
+
+$artWork[4] = array(
+	'titre' 		=> 'Tableau le numéro 12',
+	'image' 		=> 'numbers-12.jpg',
+	'description' 	=> 'Répétition de formes');
+
+/*echo"<pre>";
+print_r($artWork);
+echo"</pre>";
+exit;
+*/
+
 include 'part/header.php';
 
 ?>
 <section>
 	<h2 class="cache">Mes réalisations</h2>
+	<ul id="gallerie">
+		<?php foreach($artWork as $work) : ?><li>
+			<a href="img/<?php echo $work['image'] ?>" title="Voir <?php echo $work['titre'] ?>">
+				<img src="img/<?php echo $work['image'] ?>" alt="<?php echo $work['titre'] ?>" />
+				<p><?php echo $work['description'] ?></p>
+			</a>
+			</li><?php endforeach; ?>
+	</ul>
+	<!--
 	<ul id="gallerie">
 		<li>
 			<a href="img/numbers-01.jpg" title="Voir le tableau chiffre 1">
@@ -36,6 +78,7 @@ include 'part/header.php';
 			</a>
 		</li>
 	</ul>
+	-->
 </section>
 
 <?php include 'part/footer.php' ?>
